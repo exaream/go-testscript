f236264754083e55d1caef391f61b3925111d9b2
exaream <emctwi8@gmail.com>
2022-08-08 17:24:54 +0900

    Add gotooltest

--- sample_test.go
+++ sample_test.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/rogpeppe/go-internal/gotooltest"
 	"github.com/rogpeppe/go-internal/testscript"
 )
 
@@ -18,15 +19,33 @@ func TestMain(m *testing.M) {
 	}))
 }
 
-func TestSample(t *testing.T) {
+// TestFoo contains a test for custom command "mycat".
+func TestFoo(t *testing.T) {
+	t.Parallel()
 	testscript.Run(t, testscript.Params{
 		Dir:         filepath.Join(scriptDir, t.Name()),
 		WorkdirRoot: t.TempDir(),
 	})
 }
 
+// TestBar contains only tests for defined commands.
+// If this test contains custom commands, an error occurred in gotooltest.Setup().
+func TestBar(t *testing.T) {
+	t.Parallel()
+	params := testscript.Params{
+		Dir:         filepath.Join(scriptDir, t.Name()),
+		WorkdirRoot: t.TempDir(),
+	}
+
+	if err := gotooltest.Setup(&params); err != nil {
+		t.Fatal(err)
+	}
+
+	testscript.Run(t, params)
+}
+
 // mycat behaves like cat command.
-// Do NOT use *testing.T as an argument because mycat is used in testscript.RunMain().
+// Do NOT use *testing.T as an argument because we use mycat in testscript.RunMain().
 func mycat() int {
 	if len(os.Args) == 1 {
 		_, err := io.Copy(os.Stdout, os.Stdin)
